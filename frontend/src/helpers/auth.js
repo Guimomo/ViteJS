5c5e4e5d65ca94a79c6480479280814205828305
Guillermo Garrido <gnilleuni@gmail.com>
2025-05-31 15:11:51 -0500

    > Desarrollando Inicion de Sesion (log in)

--- frontend/src/helpers/auth.js
+++ frontend/src/helpers/auth.js
@@ -1,8 +1,11 @@
-export const setData = () => {
+export const setData = (data) => {
 
+    console.log("setData called");
+    localStorage.setItem('refreshToken', data.refreshToken);
+    localStorage.setItem('accessToken', data.accessToken);
 }
 
 
-export const getData = () => {
+export const getData = (data) => {
 
 }
--- frontend/src/views/login/loginController.js
+++ frontend/src/views/login/loginController.js
@@ -1,4 +1,5 @@
 import Swal from "sweetalert2";
+import { setData } from "../../helpers/auth";
 
 export const loginController = async () => {
 
@@ -7,7 +8,7 @@ export const loginController = async () => {
     const email = document.querySelector("#email");
     const password = document.querySelector("#password");
 
-    const log = (e) => {
+    const log = async (e) => {
 
         e.preventDefault(); // Evitar el envío del formulario por defecto
 
@@ -20,36 +21,57 @@ export const loginController = async () => {
         console.log("Valor de password:", password.value);
         console.log("Datos enviados al backend:", data);
 
-
-        fetch("http://localhost:3000/api/auth/login", {
+        const solicitud = await fetch("http://localhost:3000/api/auth/login", {
             
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
               'Content-type': 'application/json; charset=UTF-8',
             },
-        })
-        .then((response) => response.json())
-        .then((json) => {
-
-            if (json.success) {
-                Swal.fire({
-                    title: '¡Bienvenido!',
-                    text: json.message || 'Login exitoso',
-                    icon: 'success',
-                    confirmButtonText: 'Continuar'
-                }).then(() => {
-                    location.reload(); // Recargar la página
-                });
-            } else {
-                Swal.fire({
-                    title: 'Error',
-                    text: json.message || 'Credenciales incorrectas',
-                    icon: 'error',
-                    confirmButtonText: 'Intentar de nuevo'
-                });
-            }
         });
+            //.then((response) => response.json())
+        
+        // .then((json) => {
+
+        //     if (json.success) {
+        //         Swal.fire({
+        //             title: '¡Bienvenido!',
+        //             text: json.message || 'Login exitoso',
+        //             icon: 'success',
+        //             confirmButtonText: 'Continuar'
+        //         }).then(() => {
+        //             location.reload(); // Recargar la página
+        //         });
+        //     } else {
+        //         Swal.fire({
+        //             title: 'Error',
+        //             text: json.message || 'Credenciales incorrectas',
+        //             icon: 'error',
+        //             confirmButtonText: 'Intentar de nuevo'
+        //         });
+        //     }
+        // });
+
+        const respuestaLogin = await solicitud.json();
+        if (respuestaLogin.success) {
+            Swal.fire({
+                title: '¡Bienvenido!',
+                text: respuestaLogin.message || 'Login exitoso',
+                icon: 'success',
+                confirmButtonText: 'Continuar'
+            }).then(() => {
+                location.reload(); // Recargar la página
+            });
+
+            setData(respuestaLogin.data);
+        } else {
+            Swal.fire({
+                title: 'Error',
+                text: respuestaLogin.message || 'Credenciales incorrectas',
+                icon: 'error',
+                confirmButtonText: 'Intentar de nuevo'
+            });
+        }
     
         form.reset(); // Limpiar el formulario
 
